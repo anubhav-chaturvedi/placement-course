1ea7fc6918022aa822446112fc9a1e03534ea9a1
AnubhavChaturvedi <48079486+anubhav-chaturvedi@users.noreply.github.com>
2019-07-26 12:05:30 +0530

    Add files via upload
    first two programs taught in video 3

---
+++ operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=12,b=10,z;
+    float f=3.5;
+    char c='A';
+    double d=3.56798;
+    cout<<a<<endl;
+    //a=a+b;
+    cin>>a>>b>>d;
+    z=a%b;
+    cout<<a<<" "<<b<<" "<<f<<" "<<z;
+    return 0;
+}
