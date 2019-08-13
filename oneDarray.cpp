8e11d0ba2612785bdf6170e146ae5e63041356df
AnubhavChaturvedi <48079486+anubhav-chaturvedi@users.noreply.github.com>
2019-08-13 13:06:59 +0530

    Lecture-9

---
+++ oneDarray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    for(int i=0; i < 5; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i < 5; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
